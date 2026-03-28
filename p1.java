3dda94139055bfab07979d6c582e4d8b70f638fc
AquaCodes02 <trackpixel0@gmail.com>
2026-03-29 00:07:23 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,13 @@
+import java.util.* ;
+public class p1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in) ;
+        int x, n, sum=0 ;
+        x = sc.nextInt() ;
+        n = sc.nextInt() ;
+        for(int i=1 ; i<=n ; i++) {
+            sum = sum + (int)Math.pow(x,n) ;            
+        }
+        System.out.println("The sum of the series is = " +sum) ;
+    }
+}
