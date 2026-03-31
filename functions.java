de345c1cc2824dbb3abc083d92af2baae1a6819f
AquaCodes02 <trackpixel0@gmail.com>
2026-03-31 11:46:02 +0530

    Add files via upload
    A simple java snippet protraying calling, defining and passing arguments to a function.

---
+++ functions.java
@@ -0,0 +1,9 @@
+public class functions {
+    public static int add(int a, int b) {
+    return (a+b) ;
+    }
+    public static void main(String args[]) {
+        int sum = add(40,70) ; 
+        System.out.println("The sum of two numbers is = " +sum) ;
+    }
+}
